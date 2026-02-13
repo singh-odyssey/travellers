0401a1348ccf1c4a48c33ea03171d804e638bc5d
Shivansh Mathur <101795319+ShivanshCoding36@users.noreply.github.com>
2026-02-13 20:54:47 +0530

    Update page.tsx

--- src/app/routes/new/page.tsx
+++ src/app/routes/new/page.tsx
@@ -131,7 +131,7 @@ export default function NewRoutePage() {
       <main className="min-h-screen py-16 px-6 ">
         
         <div className="mx-auto max-w-[1800px]">  
-        <h1 className="text-2xl font-semibold">Sign in to Create Route</h1>
+        <h1 className="text-2xl font-semibold">Sign in to Create Routes</h1>
         <p className="mt-2 text-slate-600 ">You need an account to Create, View, and Manage Routes.</p>
           <div className="mt-6">
               <SignInForm />
