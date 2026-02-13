d959c1e39f6398f0cf4548aac350e109931f9186
Shivansh Mathur <101795319+ShivanshCoding36@users.noreply.github.com>
2026-02-13 20:53:16 +0530

    Update page.tsx

--- src/app/routes/page.tsx
+++ src/app/routes/page.tsx
@@ -68,8 +68,8 @@ export default function RoutesPage() {
       <main className="min-h-screen py-16 px-6 ">
         
         <div className="mx-auto max-w-[1800px]">  
-        <h1 className="text-2xl font-semibold">Sign in to upload</h1>
-        <p className="mt-2 text-slate-600 ">You need an account to upload and verify tickets.</p>
+        <h1 className="text-2xl font-semibold">Sign in to Create Route</h1>
+        <p className="mt-2 text-slate-600 ">You need an account to Create, View, and Manage Routes.</p>
           <div className="mt-6">
               <SignInForm />
               </div>
